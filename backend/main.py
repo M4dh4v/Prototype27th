11b785c908a5bb92d4b589c1a7c070d9e1e4858a
M4dh4v <kbsmadhav0923@gmail.com>
2025-09-16 19:13:51 +0530

    added database connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pymongo.server_api import ServerApi
+from pymongo.mongo_client import MongoClient
 
 app = FastAPI()
+uri = "mongodb+srv://discordboosterm_db_user:WaawNoiceCluster@cluster0.czjyvje.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 @app.get('/')
 def root():
